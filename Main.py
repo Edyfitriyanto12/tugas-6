5a3ebc78b00a62064df1cced5415c8687f361a95
Edy fitriyanto <edyfitriyant0o12@gmail.com>
2023-10-22 19:56:50 +0700

    menambahkan fungsi perkalian dan pembagian

--- Main.py
+++ Main.py
@@ -1,18 +1,27 @@
-from math_function import add
+# from math_function import add
 
 
-def main():
+# def main():
 
-    data_1 = int(input("masukkan input 1 :"))
-    data_2 = int(input("masukkan input 2 :"))
-    operator = input("masukkan operator :")
+#     data_1 = int(input("masukkan input 1 :"))
+#     data_2 = int(input("masukkan input 2 :"))
+#     operator = input("masukkan operator :")
 
-    if operator == "+":
-        result = add(data_1, data_2)
+#     if operator == "+":
+#         result = add(data_1, data_2)
 
-    print("{} {} {} = {} ".format(data_1, operator, data_2, result))
+#     print("{} {} {} = {} ".format(data_1, operator, data_2, result))
 
 
-if __name__ == "__main__":
-    print("Hello Main !")
-    main()
+# if __name__ == "__main__":
+#     print("Hello Main !")
+#     main()
+
+#program 2
+from math_function import multiply, divide
+
+hasil_perkalian = multiply(10, 5)
+print("Hasil Perkalian:", hasil_perkalian)
+
+hasil_pembagian = divide(10, 2)
+print("Hasil Pembagian:", hasil_pembagian)
--- Math_function.py
+++ Math_function.py
@@ -1,3 +1,12 @@
 
-def add(a, b):
-    return a + b
+# def add(a, b):
+#     return a + b
+
+#program 2
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ZeroDivisionError("Tidak bisa dibagi dengan nol")
+    return a / b
